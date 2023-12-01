875b0859e8abd4524301eea466e63bb14acd5ade
12manel123 <manelcastellvicerezuela@gmail.com>
2023-12-01 13:41:20 +0100

    Calculadora creada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,32 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ta37';
+  valorEnPantalla: string = '';
+  resultadoEnPantalla: string = '';
+  primerOperando: number | null = null;
+  operacion: string[] = [];
+  agregarAlValorEnPantalla(valor: string): void {
+    this.valorEnPantalla += valor;
+  }
+
+  borrarDelValorEnPantalla(): void {
+    this.valorEnPantalla = this.valorEnPantalla.slice(0, -1);
+  }
+
+  limpiarPantalla(): void {
+    this.valorEnPantalla = '';
+    this.operacion = [];
+  }
+
+  realizarOperacion(operacion: string): void {
+    this.operacion.push(this.valorEnPantalla, operacion);
+    this.valorEnPantalla = '';
+  }
+
+  calcularResultado(): void {
+    this.operacion.push(this.valorEnPantalla);
+    const expresion = this.operacion.join(' ');
+    this.valorEnPantalla = eval(expresion).toString();
+    this.operacion = [];
+  }
 }
